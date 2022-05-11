780180570ef39944fa706dd990befe4556877827
Babatunde Owoleke <tundeowoleke1@gmail.com>
2022-05-11 16:54:27 +0100

    Implement UI for Post Detail screen.

---
+++ app/src/main/java/com/tunde/techcognitosocial/ui/main/PostDetailFragment.kt
@@ -0,0 +1,26 @@
+package com.tunde.techcognitosocial.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.tunde.techcognitosocial.R
+import com.tunde.techcognitosocial.databinding.FragmentPostDetailBinding
+
+
+class PostDetailFragment : Fragment() {
+
+    private lateinit var binding: FragmentPostDetailBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentPostDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+}
